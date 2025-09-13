d3d8b8676c829ecdd80fc551bc18ec2882f9e485
blakej23 <blakejones1991@gmail.com>
2025-09-13 20:13:38 +0000

    create board form working

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,4 +9,14 @@ class BoardsController < ApplicationController
     render({ :template => "board_templates/show"})
   end
 
+  def create
+    @new_board = Board.new
+    @new_board.name = params.fetch("name_param")
+    @new_board.save
+
+    
+  render({ :template => "board_templates/create_confirm"})
+  end
+
+
 end
