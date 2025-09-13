fb7f36cd8dc7ebfd3a3d8b4d2998d0cf248a9dde
blakej23 <blakejones1991@gmail.com>
2025-09-13 22:05:46 +0000

    New listing working

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,18 @@
+class ListingsController < ApplicationController
+
+  def create
+    # create a new listing
+    # populates its attributes
+
+    new_listing = Listing.new
+    
+    new_listing.title = params.fetch("title_param")
+    new_listing.body = params.fetch("body_param")
+    new_listing.expires_on = params.fetch("expires_on_param")
+    new_listing.board_id = params.fetch("board_id_param")
+    new_listing.save
+
+    redirect_to("/boards/#{new_listing.board_id}")
+
+  end
+end
